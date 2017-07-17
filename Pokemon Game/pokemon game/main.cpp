3f7ff2c841bb4f2e3de0d9e5af9bcc81b08ca97f
ppolicherla <pavan.policherla1991@gmail.com>
2017-07-16 20:03:37 -0400

    updated the files

---
+++ Pokemon Game/pokemon game/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	//initialize the health of the player and the opponent
+	int player_health = 100;
+	int cpu_health = 100;
+	//Player move names
+	string move1 = "burn";
+	string move2 = "slash";
+	string move3 = "heal";//will be used for both player and cpu
+	//Cpu move names
+	string move4 = "smash";
+	string move5 = "crush";
+	//game end message
+	string cpu_wins = "Game over you lost :(";
+	string u_win = "Game over you win!!!!!";
+
+
+
+
+	return 0;
+}
